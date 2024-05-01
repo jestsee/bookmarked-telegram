89fee4257339a7d803546b56f233e866fa2f1bb1
Jesica <jestsee@gmail.com>
2024-05-01 14:26:06 +0700

    chore: update auth command

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,22 @@ import { getTweetUrl } from './utils';
 import { WizardEnum } from './constants/enum';
 import { main } from './webhook';
 import { pool } from './db/pool';
+import { getToken } from './db/get-token';
 
-bot.command('auth', (ctx) => {
+bot.command('auth', async (ctx) => {
   if (ctx.session?.accessToken) {
     return ctx.reply('You have already authenticated');
   }
+
+  const accessToken = await getToken(ctx.from?.id.toString() ?? '');
+  if (accessToken) {
+    ctx.session = { accessToken };
+    return ctx.reply('You have already authenticated');
+  }
+
   const userId = ctx.update.message.from.id.toString();
   const botUsername = ctx.botInfo.username;
+
   return ctx.reply(
     'You will be directed to an external website for sign in.',
     authKeyboard(botUsername, userId)
@@ -22,11 +31,16 @@ bot.start(async (ctx) => {
   if (ctx.payload && ctx.payload === 'success') {
     return ctx.reply('Successfully authenticated 🎉');
   }
+
+  return ctx.reply(
+    'Hello! 👋🏼 To start bookmarking tweets, you need to be authenticated first'
+  );
 });
 
 bot.on(message('text'), async (ctx) => {
   const tweetUrl = getTweetUrl(ctx.text);
   if (tweetUrl) {
+    await ctx.scene.leave(); // leave the current scene
     ctx.scene.session.bookmarkPayload = { url: tweetUrl };
     return ctx.scene.enter(WizardEnum.BOOKMARK);
   }
